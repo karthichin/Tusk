0f024cec94449ca7b72fd61de15a6979231db00a
karthick kumar.N <61644657+karthichin@users.noreply.github.com>
2020-03-01 19:32:20 +0530

    new car details updated

--- karthick.py
+++ karthick.py
@@ -2,4 +2,6 @@
 
 Print "This is a first GIT file i created here "
 Print "so i establish a little old python code to test"
-
+cars = Hyundai creta 2016 petrol
+price = 600000
+deal = immediate close
